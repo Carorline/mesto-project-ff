51fbfb86018f271de43fb291bbeb851bca269a2b
Valentina Osinceva <Carorline@yandex.ru>
2025-04-17 12:02:51 +0300

    function createCard, deleteCard,appendCard

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,31 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+// @todo: Функция создания карточки
+function createCard(contentCard, deleteCallback) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  cardElement.querySelector(".card__title").textContent = contentCard.name;
+  cardElement.querySelector(".card__image").src = contentCard.link;
+  cardElement.querySelector(".card__image").alt = contentCard.name;
+  
+  const buttonDelete = cardElement.querySelector(".card__delete-button");
+  buttonDelete.addEventListener("click", () => {deleteCallback(cardElement);});
+
+  return cardElement;
+}
+
+// @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// @todo: Вывести карточки на страницу
+const listPlaces = document.querySelector(".places__list");
+
+initialCards.forEach((contentCard) => {
+  const cardElement = createCard(contentCard, deleteCard);
+  listPlaces.append(cardElement);
+});
